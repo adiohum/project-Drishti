e66304e29694490ff80025934bd81336d44e7f28
adiohum <xifer545@gmail.com>
2026-02-26 00:34:14 +0530

    Update camera.js

--- camera.js
+++ camera.js
@@ -1,45 +1,25 @@
-
-let model, stream;
-let detecting = false;
+let stream = null;
 
 const video = document.getElementById("video");
 const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
 
 async function startSystem() {
-    try {
-        model = await cocoSsd.load();
+  stream = await navigator.mediaDevices.getUserMedia({
+    video: { facingMode: "environment" }
+  });
 
-        stream = await navigator.mediaDevices.getUserMedia({
-            video: {
-                facingMode: { ideal: "environment" }
-            }
-        });
+  video.srcObject = stream;
 
-        video.srcObject = stream;
-
-        video.onloadedmetadata = () => {
-            canvas.width = video.videoWidth;
-            canvas.height = video.videoHeight;
-            detecting = true;
-            objectStates = {};
-            clearLog();
-            startDetection();
-        };
-
-    } catch (err) {
-        alert("Camera access denied or not supported.");
-        console.error(err);
-    }
+  video.onloadedmetadata = () => {
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    loadModel();   // call detection
+  };
 }
 
 function stopSystem() {
-    detecting = false;
-
-    if (stream) {
-        stream.getTracks().forEach(track => track.stop());
-    }
-
-    stopVoice();
-    objectStates = {};
+  if (stream) {
+    stream.getTracks().forEach(track => track.stop());
+  }
+  stopDetection();
 }
